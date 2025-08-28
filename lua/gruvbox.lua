f295d8629c382b8db5bc80aa7236f198ef1fe994
Motaz Shokry <motaz-dawood@tutamail.com>
2025-08-28 14:21:43 +0300

    adding his

--- lua/gruvbox.lua
+++ lua/gruvbox.lua
@@ -391,6 +391,11 @@ local function set_highlights()
     --- Plugins
     -- OXY2DEV/markview.nvim
     MarkviewPalette1 = { fg = palette.red_dark, bg = "#3B1F1F" },
+    MarkviewPalette2 = { fg = palette.yellow_dark, bg = "#1F2A2F" },
+    MarkviewPalette3 = { fg = palette.green_dark, bg = "#2F2F1F" },
+    MarkviewPalette4 = { fg = palette.purple_dark, bg = "#2F1F28" },
+    MarkviewPalette5 = { fg = palette.blue_dark, bg = "#1F2A2F" },
+    MarkviewPalette6 = { fg = palette.aqua_dark, bg = "#1F2F23" },
 
     -- romgrk/barbar.nvim
     BufferCurrent = { fg = palette.fg, bg = palette.bg_main },
